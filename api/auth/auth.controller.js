67c8ee97be51741f4a853e71e03b3af5d9fed05d
Ido Halbany <idohalbany97@gmail.com>
2023-10-01 21:39:02 +0300

    user backend update

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -19,14 +19,15 @@ export async function login(req, res) {
 }
 
 export async function signup(req, res) {
+  console.log('req.body', req.body)
   try {
-    const { username, password, fullname } = req.body
+    const { username, password, fullname, score, isAdmin } = req.body
 
     // IMPORTANT!!!
     // Never write passwords to log file!!!
     // logger.debug(fullname + ', ' + username + ', ' + password)
 
-    const account = await authService.signup(username, password, fullname)
+    const account = await authService.signup(username, password, fullname, score, isAdmin)
     logger.debug(`auth.route - new account created: ` + JSON.stringify(account))
 
     const user = await authService.login(username, password)
